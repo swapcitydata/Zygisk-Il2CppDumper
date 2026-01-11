7441f171b53336ebd992d593f707bab19f32ae95
swapcitydata <swapnil.manyar31599@gmail.com>
2026-01-11 18:27:42 +0530

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,10 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+// Replace the placeholder with the actual package name
+#define GamePackageName "com.ea.gp.fifamobile"
+
+// Add this line to target the correct EA library
+#define LibraryName "libFIFAMobileNeon.so"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
